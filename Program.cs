f9e386d7da4d85eebed62960ea9c7791e4aba61f
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2021-04-19 16:27:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractionDemo a1 = new AbstractionDemo();
+            a1.CarName = "BMW";
+            a1.CarColor = "Metailc grey";
+            a1.Steering();
+            a1.Brakes();
+            a1.Gear();
+        }
+    }
+}
